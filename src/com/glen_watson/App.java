4997bc80b3463354e1812659f485a204e46219ef
glen.watson <glen.watson@alumni.neumont.edu>
2014-03-14 20:53:37 -0400

    adds comments and meaningful defaults

--- src/com/glen_watson/App.java
+++ src/com/glen_watson/App.java
@@ -11,60 +11,112 @@ import android.widget.Toast;
 
 public class App {
 	
-	protected static final String TAG = "com.glen_watson";
+	/*
+	 * Override this with your app-specific tag
+	 */
+	protected static final String TAG = App.class.getPackage().toString();
 	
-	protected static final boolean debug = false;
+	/**
+	 * Returns if a debugger is connected to the device
+	 * Don't use this for application logic!
+	 */
+	protected static final boolean isDebugging = android.os.Debug.isDebuggerConnected() || android.os.Debug.waitingForDebugger();
 	
+	/**
+	 * Creates a short toast
+	 * @param c
+	 * @param msg
+	 */
 	public static void toast(Context c, String msg) {
 		shortToast(c, msg);
 	}
 	
-	public static void shortToast(Context c, String msg) {
-		Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+	/**
+	 * @see android.widget.Toast#makeText(Context, CharSequence, int)
+	 * @param c
+	 * @param msg
+	 */
+	public static void shortToast(Context c, Object obj) {
+		Toast.makeText(c, obj+"", Toast.LENGTH_SHORT).show();
 	}
 	
-	public static void longToast(Context c, String msg) {
-		Toast.makeText(c, msg, Toast.LENGTH_LONG).show();
+	/**
+	 * @see android.widget.Toast#makeText(Context, CharSequence, int)
+	 * @param c
+	 * @param msg
+	 */
+	public static void longToast(Context c, Object obj) {
+		Toast.makeText(c, obj+"", Toast.LENGTH_LONG).show();
 	}
 	
+	/**
+	 * @see android.util.Log#d(String, String)
+	 */
 	public static void d(Object obj) {
 		Log.d(TAG, obj+"");
 	}
 	
+	/**
+	 * @param obj - the object to log
+	 * @param condition - only log if this is true
+	 * @see android.util.Log#d(String, String)
+	 */
 	public static void d(Object obj, boolean condition) {
 		if(condition)
 			Log.d(TAG, obj+"");
 	}
 	
+	/**
+	 * @see android.util.Log#i(String, String)
+	 */
 	public static void i(Object obj) {
 		Log.i(TAG, obj+"");
 	}
 	
+	/**
+	 * @see android.util.Log#v(String, String)
+	 */
 	public static void v(Object obj) {
 		Log.v(TAG, obj+"");
 	}
 	
+	/**
+	 * @see android.util.Log#v(String, String, Throwable)
+	 */
 	public static void v(Object obj, Throwable t) {
 		Log.v(TAG, obj+"", t);
 	}
 	
+	/**
+	 * @see android.util.Log#w(String, String)
+	 */
 	public static void w(Object obj) {
 		Log.w(TAG, obj+"");
 	}
 	
+	/**
+	 * @see android.util.Log#w(String, String, Throwable)
+	 */
 	public static void w(Object obj, Throwable t) {
 		Log.w(TAG, obj+"", t);
 	}
 	
+	/**
+	 * @see android.util.Log#e(String, String)
+	 */
 	public static void e(Object obj) {
 		Log.e(TAG, obj+"");
 	}
 	
+	/**
+	 * @see android.util.Log#e(String, String, Throwable)
+	 */
 	public static void e(Object obj, Throwable t) {
 		Log.e(TAG, obj+"", t);
 	}
 	
 	/**
+	 * Gets the approximate location of the device
 	 * @param context
 	 * @return - null, if no location was found; else returns the most accurate location
 	 */
@@ -93,6 +145,11 @@ public class App {
 	}
 	
 	public static class Debug {
+		/**
+		 * @param action - a android.view.MotionEvent
+		 * @return - the String representation of action
+		 * @see android.view.MotionEvent
+		 */
 		public static String actionToString(int action) {
 			switch(action) {
 				case MotionEvent.ACTION_DOWN:
